848ff32a1b36470cdd4d489395e1932ee9d047f1
hanyi <2387870957@qq.com>
2022-07-14 17:59:06 +0800

    添加原型

--- src/jquery.js
+++ src/jquery.js
@@ -19,116 +19,122 @@ window.$ = window.jQuery = function (selectorOrArrayOrTemplate) {
     return container.content.firstChild;
   }
 
-  // 返回一个可以直接操作元素elements的对象api
-  return {
-    jquery: true, // ?
-    elements: elements, // ?
-    oldApi: selectorOrArrayOrTemplate.oldApi,
-
-    // 增
-    appendTo(node) {
-      if (node instanceof Element) {
-        this.each((el) => node.appendChild(el));
-        // 遍历elements，对每个el进行node.appendChild操作
-      } else if (node.jquery == true) {
-        this.each((el) => node.get(0).appendChild(el));
-        // 遍历elements，对每个el进行node.get(0).appendChild(el)操作
-      }
-    },
-    append(children) {
-      if (children instanceof Element) {
-        this.get(0).appendChild(children);
-      } else if (children instanceof HTMLCollection) {
-        for (let i = 0; i < children; i++) {
-          this.get(0).appendChild(children[i]);
-        }
-      } else if (children.jquery === true) {
-        children.each((node) => this.get(0).appendChild(node));
-      }
-    },
-    // 删
-
-    // 改
-    // 闭包：函数访问外部变量
-    addClass(className) {
-      for (let i = 0; i < elements.length; i++) {
-        elements[i].classList.add(className);
-      }
-      return this;
-    },
-
-    // 查
-    get(index) {
-      return elements[index];
-    },
-    find(selector) {
-      let array = [];
-      for (let i = 0; i < elements.length; i++) {
-        array = array.concat(
-          Array.from(elements[i].querySelectorAll(selector))
-        );
-      }
-      /**
-       * 用之前的空数组array连接上新的元素，然后把连接了新元素的新数组array放回空数组array
-       * 另外，elements[i].querySelectorAll(selector)是一个伪数组
-       */
-      array.oldApi = this; // this是旧的api（当前的对象）
-      return jQuery(array);
-
-      /**
-       * 等价于
-       * const newApi = jQuery(array);
-       * return newApi;
-       */
-
-      /**
-       * 目的是防止
-       * elements = array;
-       * return this;
-       * "污染"elements
-       */
-    },
-    each(fn) {
-      for (let i = 0; i < elements.length; i++) {
-        fn.call(null, elements[i], i); // this为null，或者说fn不支持this
-      }
-      return this; // this是api对象
-    },
-    print() {
-      console.log(elements); // elements是对应的元素们，与this所指的对象要区分开
-    },
-    end() {
-      return this.oldApi; // this是新的api（当前的api）
-    },
-    parent() {
-      const array = [];
-      this.each((node) => {
-        // 去除重复的父亲结点
-        if (array.indexOf(node.parentNode) === -1) {
-          array.push(node.parentNode);
-        }
-      });
-      return jQuery(array); // array没有什么可操作性，所以返回一个jQuery封装后的对象
-    },
-    children() {
-      const array = [];
-      this.each((node) => {
-        array.push(...node.children);
-        /**
-         * 直接采用
-         * array.push(node.children);
-         * 得到的是一个数组的数组，不符合要求
-         * ...展开操作符的意思是将node.children拆开，第一个元素当作第一个参数，第二个元素当作第二个参数
-         * 等价于
-         * array.push(node.children[0], node.children[1], node.children[2]...)
-         */
-      });
-      return jQuery(array);
-    },
-  };
+  const api = Object.create(jQuery.prototype);
   /**
-   * 等价于
-   * const api = {}；
-   * return api;
+   * 创建一个对象，这个对象的__proto__为括号里面的东西
+   * 相当于const api = {__proto__:jQuery.prototype}
    */
+
+  Object.assign(api, {
+    elements: elements,
+    oldApi: selectorOrArrayOrTemplate.oldApi,
+  });
+  // api.elements = elements;
+  // api.oldApi = selectorOrArrayOrTemplate.oldApi;
+  return api;
+  // 返回一个可以直接操作元素elements的对象api
+};
+
+jQuery.fn = jQuery.prototype = {
+  constructor: jQuery,
+  jquery: true, // ?
+
+  // 增
+  appendTo(node) {
+    if (node instanceof Element) {
+      this.each((el) => node.appendChild(el));
+      // 遍历elements，对每个el进行node.appendChild操作
+    } else if (node.jquery == true) {
+      this.each((el) => node.get(0).appendChild(el));
+      // 遍历elements，对每个el进行node.get(0).appendChild(el)操作
+    }
+  },
+  append(children) {
+    if (children instanceof Element) {
+      this.get(0).appendChild(children);
+    } else if (children instanceof HTMLCollection) {
+      for (let i = 0; i < children; i++) {
+        this.get(0).appendChild(children[i]);
+      }
+    } else if (children.jquery === true) {
+      children.each((node) => this.get(0).appendChild(node));
+    }
+  },
+  // 删
+
+  // 改
+  // 闭包：函数访问外部变量
+  addClass(className) {
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].classList.add(className);
+    }
+    return this;
+  },
+
+  // 查
+  get(index) {
+    return elements[index];
+  },
+  find(selector) {
+    let array = [];
+    for (let i = 0; i < elements.length; i++) {
+      array = array.concat(Array.from(elements[i].querySelectorAll(selector)));
+    }
+    /**
+     * 用之前的空数组array连接上新的元素，然后把连接了新元素的新数组array放回空数组array
+     * 另外，elements[i].querySelectorAll(selector)是一个伪数组
+     */
+    array.oldApi = this; // this是旧的api（当前的对象）
+    return jQuery(array);
+
+    /**
+     * 等价于
+     * const newApi = jQuery(array);
+     * return newApi;
+     */
+
+    /**
+     * 目的是防止
+     * elements = array;
+     * return this;
+     * "污染"elements
+     */
+  },
+  each(fn) {
+    for (let i = 0; i < elements.length; i++) {
+      fn.call(null, elements[i], i); // this为null，或者说fn不支持this
+    }
+    return this; // this是api对象
+  },
+  print() {
+    console.log(elements); // elements是对应的元素们，与this所指的对象要区分开
+  },
+  end() {
+    return this.oldApi; // this是新的api（当前的api）
+  },
+  parent() {
+    const array = [];
+    this.each((node) => {
+      // 去除重复的父亲结点
+      if (array.indexOf(node.parentNode) === -1) {
+        array.push(node.parentNode);
+      }
+    });
+    return jQuery(array); // array没有什么可操作性，所以返回一个jQuery封装后的对象
+  },
+  children() {
+    const array = [];
+    this.each((node) => {
+      array.push(...node.children);
+      /**
+       * 直接采用
+       * array.push(node.children);
+       * 得到的是一个数组的数组，不符合要求
+       * ...展开操作符的意思是将node.children拆开，第一个元素当作第一个参数，第二个元素当作第二个参数
+       * 等价于
+       * array.push(node.children[0], node.children[1], node.children[2]...)
+       */
+    });
+    return jQuery(array);
+  },
 };
